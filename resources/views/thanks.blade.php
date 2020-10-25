e80e655b3ee5bd6d7b85a642aa8f8be188945a20
Jamie Peters <jamie@jamie-peters.co.uk>
2020-10-25 14:20:05 +0000

    update thanks page

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -10,82 +10,82 @@
         </p>
 
         <p>
-            <strong>Afternoon Reception</strong>, including lunch and drinks - with thanks to <a
+            <strong>Afternoon Reception</strong>, including lunch and drinks - <a
                 href="https://cheshirecatnantwich.co.uk/" target="_blank">The Cheshire Cat, Nantwich</a>.
         </p>
 
         <p>
-            <strong>Balloons</strong> - with thanks to <a href="https://www.facebook.com/BeesKneesBalloons"
+            <strong>Balloons</strong> - <a href="https://www.facebook.com/BeesKneesBalloons"
                                                           target="_blank">The Bees Knees</a>.
         </p>
 
         <p>
-            <strong>Band and Music</strong> - with thanks to <a href="https://funtimefrankies.co.uk/" target="_blank">The
+            <strong>Band and Music</strong> - <a href="https://funtimefrankies.co.uk/" target="_blank">The
                 Funtime Frankies</a>
         </p>
 
         <p>
-            <strong>Best Man</strong> - with thanks to Daniel Peters.
+            <strong>Best Man</strong> - Daniel Peters.
         </p>
 
         <p>
-            <strong>Bridesmaids</strong> - with thanks to Lara Bailey, Katie Manuel and Sophie User.
+            <strong>Bridesmaids</strong> - Lara Bailey, Katie Manuel and Sophie User.
         </p>
 
         <p>
-            <strong>Bridal Party Hotel</strong> - with thanks to <a href="https://cheshirecatnantwich.co.uk/"
+            <strong>Bridal Party Hotel</strong> - <a href="https://cheshirecatnantwich.co.uk/"
                                                                     target="_blank">The Cheshire Cat, Nantwich</a>.
         </p>
 
         <p>
-            <strong>Catering and Wedding Cake</strong> - with thanks to <a
+            <strong>Catering and Wedding Cake</strong> - <a
                 href="https://www.facebook.com/willowcottagekitchen/" target="_blank">Willow Cottage Kitchen</a>.
         </p>
 
         <p>
-            <strong>Ceremony</strong> - with thanks to <a
+            <strong>Ceremony</strong> - <a
                 href="http://www.civicvenuescheshire.co.uk/weddings-civil-ceremonies-at-crewe-municpal-buildings/"
                 target="_blank">Crewe Municipal Buildings</a> and Cheshire East Council.
         </p>
 
         <p>
-            <strong>Evening Reception</strong>, including tables, table covers, and chairs - with thanks to <a
+            <strong>Evening Reception</strong>, including tables, table covers, and chairs - <a
                 href="https://www.nantwichtownfc.co.uk/function-suite/" target="_blank">The Function suite at Nantwich
                 Town FC</a>.
         </p>
 
         {{--        <p>--}}
-        {{--            <strong>Flowers</strong> - with thanks to <a href="" target="_blank">...</a>.--}}
+        {{--            <strong>Flowers</strong> - <a href="" target="_blank">...</a>.--}}
         {{--        </p>--}}
 
         {{--        <p>--}}
-        {{--            <strong>Hair</strong> - with thanks to <a href="" target="_blank">...</a>.--}}
+        {{--            <strong>Hair</strong> - <a href="" target="_blank">...</a>.--}}
         {{--        </p>--}}
 
         <p>
-            <strong>Invitations</strong> - with thanks to Jamie Peters for designing and
+            <strong>Invitations</strong> - Jamie Peters for designing and
             <a href="https://www.digitalprinting.co.uk/" target="_blank">Digital Printing UK</a> for printing.
         </p>
 
         <p>
-            <strong>Limo Hire</strong> - with thanks to <a href="https://www.limo-sceneuk.co.uk/" target="_blank">Limo-Scene</a>.
+            <strong>Limo Hire</strong> - <a href="https://www.limo-sceneuk.co.uk/" target="_blank">Limo-Scene</a>.
         </p>
 
         {{--        <p>--}}
-        {{--            <strong>Make Up</strong> - with thanks to <a href="" target="_blank">...</a>.--}}
+        {{--            <strong>Make Up</strong> - <a href="" target="_blank">...</a>.--}}
         {{--        </p>--}}
 
         <p>
-            <strong>Photography</strong> - with thanks to <a href="https://www.thorneweddingphotography.co.uk/"
+            <strong>Photography</strong> - <a href="https://www.thorneweddingphotography.co.uk/"
                                                              target="_blank">Jon Thorne Photography</a>.
         </p>
 
         <p>
-            <strong>Sweets</strong> - with thanks to <a href="https://www.curlycow.co.uk" target="_blank">Curly Cow</a>.
+            <strong>Sweets</strong> - <a href="https://www.curlycow.co.uk" target="_blank">Curly Cow</a>.
         </p>
 
         <p>
-            <strong>Website</strong> - with thanks to Jamie Peters.
+            <strong>Website</strong> - Jamie Peters.
         </p>
     </div>
 @endsection
